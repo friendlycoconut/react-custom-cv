890d91038ade64e7ec56d45aa44403cd33377a3b
friendlycoconut <kosten.il2000@gmail.com>
2024-08-19 17:36:53 +0200

    fix: added fix for React app Git pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}>
+        <Route path="./" element={<Home/>}>
         </Route>
       </Routes>  
     </BrowserRouter>
